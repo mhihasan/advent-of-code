26ae55031e2c068cc623ad2c89a9a3fb83879c1e
mhihasan <hasanuli10@gmail.com>
2024-12-01 00:19:18 -0500

    Add 2024-day01 solution

--- puzzles/year_2024/day01/solution.py
+++ puzzles/year_2024/day01/solution.py
@@ -1,4 +1,6 @@
 import os
+import re
+from collections import Counter
 
 
 def read_input(file_name: str) -> list[str]:
@@ -12,18 +14,38 @@ def parse_input(file_name):
     return inputs
 
 
-def solve_part1(inputs):
-    total = 0
+def read_lists(inputs):
+    list_a = []
+    list_b = []
+
+    pattern = re.compile(r"\s*(\d+)\s+(\d+)\s*")
+
     for line in inputs:
-        print(line)
-    return total
+        match = pattern.match(line)
+        item_a, item_b = match.groups()
+        list_a.append(int(item_a))
+        list_b.append(int(item_b))
+    return list_a, list_b
+
+
+def solve_part1(inputs):
+    list_a, list_b = read_lists(inputs)
+    sorted_list_a = sorted(list_a)
+    sorted_list_b = sorted(list_b)
+    distance = 0
+    for i in range(len(sorted_list_a)):
+        distance += abs(sorted_list_a[i] - sorted_list_b[i])
+    return distance
 
 
 def solve_part2(inputs):
-    total = 0
-    for line in inputs:
-        print(line)
-    return total
+    similarity_score = 0
+    list_a, list_b = read_lists(inputs)
+    list_b_counts_by_items = Counter(list_b)
+    for item in list_a:
+        count_in_list_b = list_b_counts_by_items[item]
+        similarity_score += count_in_list_b * item
+    return similarity_score
 
 
 def solve(file_name, part=1):
@@ -35,4 +57,4 @@ def solve(file_name, part=1):
 
 
 if __name__ == "__main__":
-    solve("demo_input.txt", part=1)
+    solve("input.txt", part=2)
