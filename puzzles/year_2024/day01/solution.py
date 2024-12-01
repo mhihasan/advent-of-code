5392dfebe3cbf8f503145bb299b4a316fafc26e3
mhihasan <hasanuli10@gmail.com>
2024-11-30 23:53:02 -0500

    Add dayone draft

---
+++ puzzles/year_2024/day01/solution.py
@@ -0,0 +1,38 @@
+import os
+
+
+def read_input(file_name: str) -> list[str]:
+    file_path = os.path.join(os.path.dirname(__file__), file_name)
+    with open(file_path, "r") as f:
+        return f.read().splitlines()
+
+
+def parse_input(file_name):
+    inputs = read_input(file_name)
+    return inputs
+
+
+def solve_part1(inputs):
+    total = 0
+    for line in inputs:
+        print(line)
+    return total
+
+
+def solve_part2(inputs):
+    total = 0
+    for line in inputs:
+        print(line)
+    return total
+
+
+def solve(file_name, part=1):
+    inputs = parse_input(file_name)
+    if part == 1:
+        return solve_part1(inputs)
+
+    return solve_part2(inputs)
+
+
+if __name__ == "__main__":
+    solve("demo_input.txt", part=1)
